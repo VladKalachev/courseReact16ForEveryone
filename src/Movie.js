9a48c3951f982b2146b2a11bd177c3df4d076026
kalachev_va <kalachev_va@ursip.ru>
2018-10-17 19:33:21 +0300

    add function comoinent

--- src/Movie.js
+++ src/Movie.js
@@ -1,22 +1,38 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-class Movie extends Component {
-  static propTypes = {
-    movie: PropTypes.shape({
-      title: PropTypes.string.isRequired
-    }),
-    disc: PropTypes.string
-  };
+// class Movie extends Component {
+//   static propTypes = {
+//     movie: PropTypes.shape({
+//       title: PropTypes.string.isRequired
+//     }),
+//     disc: PropTypes.string
+//   };
 
-  render() {
-    return (
-      <div>
-        <h1>{this.props.movie.title}</h1>
-        <p>{this.props.movie.overview}</p>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.props.movie.title}</h1>
+//         <p>{this.props.movie.overview}</p>
+//       </div>
+//     );
+//   }
+// }
+
+const Movie = ({ movie }) => {
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+      <p>{movie.overview}</p>
+    </div>
+  );
+};
 
 export default Movie;
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired
+  }),
+  disc: PropTypes.string
+};

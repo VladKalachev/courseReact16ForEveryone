3e6670c36ab043891360881c922729a7442c311a
kalachev_va <kalachev_va@ursip.ru>
2018-10-17 18:54:56 +0300

    add async fetch

--- src/App.js
+++ src/App.js
@@ -3,33 +3,33 @@ import logo from "./logo.svg";
 import "./App.css";
 import Movie from "./Movie";
 
-const movies = [
-  {
-    id: 1,
-    title: "Harry Potter"
-  },
-  {
-    id: 2,
-    title: "Spider Man",
-    disc: "desc"
-  },
-  {
-    id: 3,
-    title: "36th Chamber of Shaolin"
-  },
-  {
-    id: 4,
-    title: "Riadly 1",
-    disc: "desc"
-  }
-];
+// 65e043c24785898be00b4abc12fcdaae
 
 class App extends Component {
+  state = {
+    movies: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=65e043c24785898be00b4abc12fcdaae&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+      );
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        {movies.map(movie => {
-          return <Movie key={movie.id} movie={movie} disc={movie.disc} />;
+        {this.state.movies.map(movie => {
+          return <Movie key={movie.id} movie={movie} />;
         })}
       </div>
     );
--- src/Movie.js
+++ src/Movie.js
@@ -9,32 +9,14 @@ class Movie extends Component {
     disc: PropTypes.string
   };
 
-  static defaultProps = {
-    disc: "description not available"
-  };
-
   render() {
-    console.log(this.props);
     return (
       <div>
         <h1>{this.props.movie.title}</h1>
-        <p>{this.props.disc}</p>
+        <p>{this.props.movie.overview}</p>
       </div>
     );
   }
 }
-
-// function Movie({ movie }) {
-//   return (
-//     <div>
-//       <h1>{movie.title}</h1>
-//       <p>{movie.title}</p>
-//     </div>
-//   );
-// }
-
-// Movie.PropTypes = {
-
-// }
 
 export default Movie;
